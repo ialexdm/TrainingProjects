0f5fd731c9f502eadc33b928a6b1ccfe2453ec71
ialexdm <ivashininad@gmail.com>
2020-09-25 12:49:31 +0300

    task 2 complete

--- GeekBrains/Java/Level1/src/main/java/Lesson1/Main.java
+++ GeekBrains/Java/Level1/src/main/java/Lesson1/Main.java
@@ -1,4 +1,16 @@
 package Lesson1;
 
 public class Main {
+    //1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
+
+    //2. Создать переменные всех пройденных типов данных, и инициализировать их значения;
+    byte b = 100;
+    short s = 10000;
+    int i = 1000000000;
+    long l = 1000000000000000000L;
+    float f = 0.001f;
+    double d = 0.000000000000009;
+    char c = 'C';
+    boolean bool = true;
+    String str = "String";
 }
