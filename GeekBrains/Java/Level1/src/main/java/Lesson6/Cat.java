9212fb997d202c28edcc560a029a98d120bb3423
ialexdm <ivashininad@gmail.com>
2020-10-21 12:08:35 +0300

    Update class Animal, update class Cat

--- GeekBrains/Java/Level1/src/main/java/Lesson6/Cat.java
+++ GeekBrains/Java/Level1/src/main/java/Lesson6/Cat.java
@@ -1,4 +1,12 @@
 package Lesson6;
 
-public class Cat {
+public class Cat extends Animal{
+
+    public Cat(String name){
+
+    }
+
+    protected String getFormatedName() {
+        return null;
+    }
 }
