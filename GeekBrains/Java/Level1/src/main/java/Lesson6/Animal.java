ec8eb49500b2b0b489ce64a68b457d4c8af3ea96
ialexdm <ivashininad@gmail.com>
2020-10-21 12:06:39 +0300

    Update class Animal, add class Cat

--- GeekBrains/Java/Level1/src/main/java/Lesson6/Animal.java
+++ GeekBrains/Java/Level1/src/main/java/Lesson6/Animal.java
@@ -17,9 +17,36 @@ public abstract class Animal {
     protected double swimDistance;
     protected double jumpHeight;
 
-    protected abstract void run(double distance);
-    protected abstract void swim(double swimDistance);
-    protected abstract void jump(double jumpDistance);
+    protected boolean run(double distance){
+        if (distance<=runDistance){
+            System.out.println(getFormatedName() + " пробежал через препятствие!!!");
+            return true;
+        }
+        else {
+            System.out.println(getFormatedName() + " не смог пробежать препятствие.=(((");
+        }
+        return false;
+    }
+    protected boolean swim(double distance){
+        if (distance<=swimDistance){
+        System.out.println(getFormatedName() + " переплыл препятствие!!!");
+        return true;
+    }
+    else {
+        System.out.println(getFormatedName() + " не смог переплыть препятствие.=(((");
+    }
+        return false;}
+    protected boolean jump(double height){
+        if (height<=jumpHeight){
+            System.out.println(getFormatedName() + " перепрыгнул препятствие!!!");
+            return true;
+        }
+        else {
+            System.out.println(getFormatedName() + " не смог перепрыгнуть препятствие.=(((");
+        }
+        return false;
+    }
+    protected abstract String getFormatedName();
 
 
 
