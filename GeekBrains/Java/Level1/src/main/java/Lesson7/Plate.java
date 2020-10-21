cdea98d42da0cb391709fc60ddf1f178d0d9ddca
ialexdm <ivashininad@gmail.com>
2020-10-21 23:51:26 +0300

    Update Cat and Plate

--- GeekBrains/Java/Level1/src/main/java/Lesson7/Plate.java
+++ GeekBrains/Java/Level1/src/main/java/Lesson7/Plate.java
@@ -1,4 +1,47 @@
 package Lesson7;
 
 public class Plate {
+    private double capacity;
+    private double food;
+
+    public Plate(double capacity, double food) {
+        this.capacity = capacity;
+        this.food = food;
+    }
+
+    public boolean decreaseFood(double appetite){
+        if (appetite<=this.food){
+            this.food -= appetite;
+            return true;
+        }
+        return false;
+    }
+
+    public void fill(){
+        this.food = this.capacity;
+    }
+    public void fill(double food){
+        if (food> capacity - this.food){
+            this.food = this.capacity;
+        }else{
+            this.food +=food;
+        }
+
+    }
+
+    public void info(){
+        System.out.println("In plate " + food + "food.");
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public double getFood() {
+        return food;
+    }
+
+    public void setFood(double food) {
+        this.food = food;
+    }
 }
