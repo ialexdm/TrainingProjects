c6aa38e2e892b82a7fdefba47997bd81fcf482c1
ialexdm <ivashininad@gmail.com>
2020-10-21 12:24:30 +0300

    Update class Cat

--- GeekBrains/Java/Level1/src/main/java/Lesson6/Cat.java
+++ GeekBrains/Java/Level1/src/main/java/Lesson6/Cat.java
@@ -3,10 +3,25 @@ package Lesson6;
 public class Cat extends Animal{
 
     public Cat(String name){
-
+        super.name = name;
+        super.runDistance = ((int)(15000 + Math.random()*(20000-15000)))/100.00;
+        super.swimDistance = 0;
+        super.jumpHeight = ((int)(150 + Math.random()*(200-150)))/100.00;
     }
 
+    @Override
     protected String getFormatedName() {
-        return null;
+        return "Cat " + name;
+    }
+
+    @Override
+    public String toString() {
+        return getFormatedName() + " " + runDistance + " " + swimDistance + " " + jumpHeight;
+    }
+
+    @Override
+    protected boolean swim(double distance){
+        System.out.println(getFormatedName() + " can not swim");
+        return false;
     }
 }
