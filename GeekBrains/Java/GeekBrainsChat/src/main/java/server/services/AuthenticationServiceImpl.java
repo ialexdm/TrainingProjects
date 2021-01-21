3b79fc6bfe5e35519821077a679ed8fdcbde9c84
ialexdm <ivashininad@gmail.com>
2021-01-21 09:51:39 +0300

    code optimization in AuthenticationServerImpl

--- GeekBrains/Java/GeekBrainsChat/src/main/java/server/services/AuthenticationServiceImpl.java
+++ GeekBrains/Java/GeekBrainsChat/src/main/java/server/services/AuthenticationServiceImpl.java
@@ -11,7 +11,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
 
     public AuthenticationServiceImpl() {
-        this.usersList = new LinkedList<UserEntity>();
+        this.usersList = new LinkedList<>();
         usersList.add(new UserEntity("login1","pass1","Nick1"));
         usersList.add(new UserEntity("login2","pass2","Nick2"));
         usersList.add(new UserEntity("login3","pass3","Nick3"));
