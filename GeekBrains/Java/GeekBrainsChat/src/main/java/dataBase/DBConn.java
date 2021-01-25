8498d7703cc8ee2d6bd4f66d7334bbe5a601334a
ialexdm <ivashininad@gmail.com>
2021-01-25 14:27:37 +0300

    update DBConn

--- GeekBrains/Java/GeekBrainsChat/src/main/java/dataBase/DBConn.java
+++ GeekBrains/Java/GeekBrainsChat/src/main/java/dataBase/DBConn.java
@@ -1,17 +1,39 @@
 package dataBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
 public class DBConn {//Singleton
 
     private static DBConn instance;
+    private Connection conn;
 
-    private DBConn(){
+    private DBConn() throws SQLException {
+        ResourceBundle rb = ResourceBundle.getBundle("db");
+        String host = rb.getString("host");
+        String port = rb.getString("port");
+        String db = rb.getString("db");
+        String user = rb.getString("user");
+        String password = rb.getString("password");
 
+        String jdbcURL = MessageFormat.format("jdbc:mysql://{0}:{1}/{2}", host, port, db);
+        conn = DriverManager.getConnection(jdbcURL, user, password);
     }
 
     public static DBConn getInstance(){
         if (instance == null){
-            instance = new DBConn();
+            try {
+                instance = new DBConn();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
         return instance;
+    }
+    public Connection connection(){
+        return conn;
     }
 }
