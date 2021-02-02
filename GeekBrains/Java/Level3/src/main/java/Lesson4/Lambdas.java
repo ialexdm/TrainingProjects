72f04344127f36117a0ad2ddfec2297941926049
ialexdm <ivashininad@gmail.com>
2021-02-02 15:13:04 +0300

    tusks 2,3,4 complete

--- GeekBrains/Java/Level3/src/main/java/Lesson4/Lambdas.java
+++ GeekBrains/Java/Level3/src/main/java/Lesson4/Lambdas.java
@@ -1,7 +1,9 @@
 package Lesson4;
 
+import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class Lambdas {
@@ -27,12 +29,47 @@ public class Lambdas {
                 Например, «java interview» превращается в «weivretni avaj».
         public String reverse(String s)
      */
+    public String reverse(String s){
+        UnaryOperator<String> reverse = string -> {
+            StringBuilder gnirts = new StringBuilder();
+            for (int i = string.length() -2; i >=0; i--) {
+                gnirts.append(string.charAt(i));
+            }
+            return gnirts.toString();
+        };
+        return reverse.apply(s);
+    }
 /*
         3. Напишите метод, который возвращает наибольшее целое число в списке.
         public Integer maximum(Integer[] list)
-
+*/
+        public Integer maximum(Integer[] list){
+            Function<Integer[], Integer> max = array -> {
+                Integer m = array[0];
+                for (int i = 0; i < array.length; i++) {
+                    if (m<array[i]){
+                        m =array[i];
+                    }
+                }
+                return m;
+            };
+            return max.apply(list);
+        }
+    /*
         4. Напишите метод, который возвращает среднее значение из списка целых чисел.
         public Double average(List<Integer> list)
+
+        */
+        public Double average(List<Integer> list){
+            Function<List<Integer>, Double> aver = array -> {
+                Integer sum = 0;
+                for (int i = 0; i < array.size(); i++) {
+                    sum += array.get(i);
+                }
+                return (sum *1.0) /array.size();};
+            return aver.apply(list);
+        }
+    /*
 
         5. Имея список строк, напишите метод, который возвращает список всех строк, которые начинаются с буквы «а» (нижний регистр) и имеют ровно 3 буквы.
         public List<String> search(List<String> list)
