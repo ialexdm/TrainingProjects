01f70070cf5ed260253c9b6e46cfa3addee3462f
ialexdm <ivashininad@gmail.com>
2021-02-02 12:00:53 +0300

    update

--- GeekBrains/Java/GeekBrainsChat/src/main/java/client/ClientApp.java
+++ GeekBrains/Java/GeekBrainsChat/src/main/java/client/ClientApp.java
@@ -2,8 +2,6 @@ package client;
 
 import client.service.ClientService;
 
-import java.io.IOException;
-
 public class ClientApp {
     public static void main(String[] args) {
             new ClientService();
