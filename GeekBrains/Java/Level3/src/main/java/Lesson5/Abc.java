09df0831aa79529b42015bde63cb633c69bbf3d1
ialexdm <ivashininad@gmail.com>
2021-02-03 21:31:26 +0300

    create Lesson5 create Abc Class

---
+++ GeekBrains/Java/Level3/src/main/java/Lesson5/Abc.java
@@ -0,0 +1,8 @@
+package Lesson5;
+
+public class Abc {
+    public static void main(String[] args) {
+        //Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз
+        // (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
+    }
+}
