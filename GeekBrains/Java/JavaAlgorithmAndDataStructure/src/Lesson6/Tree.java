ccef31ed11ed28a3b94a22a324063d434722d23c
ialexdm <ivashininad@gmail.com>
2021-02-27 17:13:29 +0300

    update Lesson6 update class Tree create method find() create method insert()

--- GeekBrains/Java/JavaAlgorithmAndDataStructure/src/Lesson6/Tree.java
+++ GeekBrains/Java/JavaAlgorithmAndDataStructure/src/Lesson6/Tree.java
@@ -5,6 +5,64 @@ import java.text.MessageFormat;
 public class Tree {
 
 
+
+
+    private Node root;
+
+    public void insert(Weapon weapon){
+        Node node = new Node();
+        node.weapon = weapon;
+        if (root == null){
+            root = node;
+        }else {
+            Node current = node;
+            Node parent;
+            while (true){
+                parent = current;
+                if (weapon.id < current.weapon.id){
+                    current = current.leftChild;
+                    if (current == null){
+                        parent.leftChild = node;
+                        return;
+                    }
+                }else {
+                    current = current.rightChild;
+                    if (current == null){
+                        parent.rightChild = node;
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+    public Node find(int key){
+        Node current = root;
+        while (current.weapon.id!= key){
+            if (key < current.weapon.id){
+                current = current.leftChild;
+            }
+            else {
+                current = current.rightChild;
+            }
+            if (current == null){
+                return  null;
+            }
+        }
+
+
+        return current;
+    }
+
+    public boolean delete(int id){
+
+        return false;
+    }
+    public void displayTree(){
+
+    }
+
+
     private class Node{
         public Weapon weapon;
         public Node leftChild;
