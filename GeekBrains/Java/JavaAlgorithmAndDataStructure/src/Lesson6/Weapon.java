880eb54d3b27943cc5e9b01fe8845e3090dbc970
ialexdm <ivashininad@gmail.com>
2021-02-27 16:30:42 +0300

    update Lesson6 Create class Weapon create fields create constructor with all parameters

---
+++ GeekBrains/Java/JavaAlgorithmAndDataStructure/src/Lesson6/Weapon.java
@@ -0,0 +1,13 @@
+package Lesson6;
+
+public class Weapon {
+    public int id;
+    public int damage;
+    public String name;
+
+    public Weapon(int id, int damage, String name) {
+        this.id = id;
+        this.damage = damage;
+        this.name = name;
+    }
+}
