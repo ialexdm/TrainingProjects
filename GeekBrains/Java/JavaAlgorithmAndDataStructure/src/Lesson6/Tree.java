5a60fb1511421b1067cd25290799de3525b66947
ialexdm <ivashininad@gmail.com>
2021-02-27 16:56:26 +0300

    update Lesson6 update class Node create fields create method display()

--- GeekBrains/Java/JavaAlgorithmAndDataStructure/src/Lesson6/Tree.java
+++ GeekBrains/Java/JavaAlgorithmAndDataStructure/src/Lesson6/Tree.java
@@ -1,9 +1,17 @@
 package Lesson6;
 
+import java.text.MessageFormat;
+
 public class Tree {
 
 
     private class Node{
+        public Weapon weapon;
+        public Node leftChild;
+        public Node rightChild;
 
+        public void display(){
+            System.out.println(MessageFormat.format("Name: %s, Damage: %s.", weapon.name, weapon.damage));
+        }
     }
 }
